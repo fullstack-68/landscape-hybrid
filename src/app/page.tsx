8f14af0e46966e0c241c11a1e3dcec4db222a19d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-08-12 09:44:34 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,14 @@ import { getTodos } from "./actionsAndDb";
 import { FormInput } from "@/components/FormInput";
 import { TodoList } from "@/components/TodoList";
 import { Spinner } from "@/components/Spinner";
-import { use } from "react";
 import { Suspense } from "react";
+
 interface PageProps {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export default function Home({ params, searchParams }: PageProps) {
-  // const todos = await getTodos();
-
-  // const todos = use(getTodos());
-
   const todosPromise = getTodos();
   return (
     <main className="container">
@@ -21,8 +17,7 @@ export default function Home({ params, searchParams }: PageProps) {
         <h1>Todo (RSC + RCC)</h1>
       </a>
       <FormInput />
-
-      <Suspense fallback={<div>Loading Todos...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <TodoList todosPromise={todosPromise} />
       </Suspense>
       <Spinner />
